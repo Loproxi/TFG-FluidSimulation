48d8579c0e4157d5f2ec4ca69e6c66be96b50da1
Loproxi <79161178+Loproxi@users.noreply.github.com>
2024-04-10 23:23:43 +0200

    Update on Collision

--- Assets/Scripts/SimulationArea.cs
+++ Assets/Scripts/SimulationArea.cs
@@ -13,6 +13,14 @@ public struct SPHParticles
     public float pressure;
 }
 
+public struct BoundLimits
+{
+    public float ymin;
+    public float ymax;
+    public float xmin;
+    public float xmax;
+}
+
 public class SimulationArea : MonoBehaviour
 {
 
@@ -22,13 +30,24 @@ public class SimulationArea : MonoBehaviour
     public int NumParticles = 10;
     private GameObject[,] _particles;
     private Vector3[,] _velocities;
+    [SerializeField] Vector2 limits;
 
     private int NumTotalOfParticles
     {  get { return NumParticles * NumParticles; } }
 
     void Start()
     {
-        _particles = new GameObject[NumParticles,NumParticles];
+        GenerateParticles();
+    }
+
+    void Update()
+    {
+        ApplyForcesOnParticles();
+    }
+
+    private void GenerateParticles()
+    {
+        _particles = new GameObject[NumParticles, NumParticles];
         _velocities = new Vector3[NumParticles, NumParticles];
 
         for (int i = 0; i < NumParticles; ++i)
@@ -36,8 +55,8 @@ public class SimulationArea : MonoBehaviour
             for (int j = 0; j < NumParticles; ++j)
             {
 
-                _particles[i,j] = Instantiate(circle);
-                _particles[i,j].transform.localScale = new Vector3(particleScale,particleScale,particleScale);
+                _particles[i, j] = Instantiate(circle);
+                _particles[i, j].transform.localScale = new Vector3(particleScale, particleScale, particleScale);
                 Vector3 position = new Vector2(i * particleScale, j * particleScale);
                 _particles[i, j].transform.position = position;
 
@@ -45,14 +64,28 @@ public class SimulationArea : MonoBehaviour
         }
     }
 
-    void Update()
+    private void ApplyForcesOnParticles()
     {
         for (int i = 0; i < NumParticles; ++i)
         {
             for (int j = 0; j < NumParticles; ++j)
             {
-                _velocities[i, j].y += gravity * Time.deltaTime;
-                _particles[i, j].transform.position += _velocities[i, j] * Time.deltaTime;
+
+                Vector3 particlePos = _particles[i, j].transform.position;
+
+                if (particlePos.y <= limits.y)
+                {
+                    _velocities[i, j] = new Vector3(0.0f, 0.0f);
+                    _particles[i, j].transform.position.Set(particlePos.x, limits.y, particlePos.z);
+                }
+                else
+                {
+
+                    _velocities[i, j].y += gravity * Time.deltaTime;
+                    _particles[i, j].transform.position += _velocities[i, j] * Time.deltaTime;
+
+                }
+                
             }
         }
     }
