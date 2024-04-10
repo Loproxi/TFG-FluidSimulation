4a091d6b6dee8b09b56c4cbd2fa11ba4856a4fbb
Loproxi <79161178+Loproxi@users.noreply.github.com>
2024-04-10 13:59:59 +0200

    Particles Falling

--- Assets/Scripts/SimulationArea.cs
+++ Assets/Scripts/SimulationArea.cs
@@ -2,38 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
+
+public struct SPHParticles
+{
+    public Sprite mesh;
+    public Vector3 position;
+    public Vector3 velocity;
+    public float density;
+    public float pressure;
+}
 
 public class SimulationArea : MonoBehaviour
 {
 
-    struct SPHParticles
-    {
-        public Sprite mesh;
-        public Vector3 position;
-        public Vector3 velocity;
-    }
-
+    public float particleScale = 1.0f;
+    public float gravity = -9.8f;
     public GameObject circle;
     public int NumParticles = 10;
-    private SPHParticles[] _particles;
+    private GameObject[,] _particles;
+    private Vector3[,] _velocities;
+
+    private int NumTotalOfParticles
+    {  get { return NumParticles * NumParticles; } }
 
     void Start()
     {
-        _particles = new SPHParticles[NumParticles];
+        _particles = new GameObject[NumParticles,NumParticles];
+        _velocities = new Vector3[NumParticles, NumParticles];
 
-        for (int i = 0; i < NumParticles; i++)
+        for (int i = 0; i < NumParticles; ++i)
         {
-            _particles[i].mesh = circle.GetComponent<SpriteRenderer>().sprite;
-            _particles[i].position = new Vector3(i * 5, 0, 0);
-            _particles[i].velocity = Vector3.zero;
+            for (int j = 0; j < NumParticles; ++j)
+            {
 
-            Instantiate(circle, _particles[i].position, Quaternion.identity);
+                _particles[i,j] = Instantiate(circle);
+                _particles[i,j].transform.localScale = new Vector3(particleScale,particleScale,particleScale);
+                Vector3 position = new Vector2(i * particleScale, j * particleScale);
+                _particles[i, j].transform.position = position;
+
+            }
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < NumParticles; ++i)
+        {
+            for (int j = 0; j < NumParticles; ++j)
+            {
+                _velocities[i, j].y += gravity * Time.deltaTime;
+                _particles[i, j].transform.position += _velocities[i, j] * Time.deltaTime;
+            }
+        }
     }
 }
